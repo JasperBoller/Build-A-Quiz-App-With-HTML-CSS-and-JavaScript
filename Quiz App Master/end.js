65a7c98ff2d5bd80e793c06cff19b038e4e45c12
JasperBoller <JustineChristian53@gmail.com>
2025-05-05 14:31:27 +0100

    Update end.js
    Update made

--- Quiz App Master/end.js
+++ Quiz App Master/end.js
@@ -23,7 +23,8 @@ saveHighScore = (e) => {
     highScores.push(score);
     highScores.sort((a, b) => b.score - a.score);
     highScores.splice(5);
-
+    
+    localStorage.setItem('mostRecentScore', score);
     localStorage.setItem('highScores', JSON.stringify(highScores));
     window.location.assign('/');
 };
